c9bfa767c1df376753a349f6ef62ea99252fe2cb
Krista Williams <rystakei@gmail.com>
2015-02-01 15:29:18 -0500

    Added non-functional select field

--- app/scripts/reactmain.jsx
+++ app/scripts/reactmain.jsx
@@ -3,36 +3,296 @@ var Button = ReactBootstrap.Button;
 var Grid = ReactBootstrap.Grid;
 var ListGroup = ReactBootstrap.ListGroup;
 var ListGroupItem = ReactBootstrap.ListGroupItem;
-
 var ListInstance = React.createClass({
 
   getInitialState: function() {
     return {
+      countries: {
+    'AF' : 'Afghanistan',
+    'AX' : 'Aland Islands',
+    'AL' : 'Albania',
+    'DZ' : 'Algeria',
+    'AS' : 'American Samoa',
+    'AD' : 'Andorra',
+    'AO' : 'Angola',
+    'AI' : 'Anguilla',
+    'AQ' : 'Antarctica',
+    'AG' : 'Antigua And Barbuda',
+    'AR' : 'Argentina',
+    'AM' : 'Armenia',
+    'AW' : 'Aruba',
+    'AU' : 'Australia',
+    'AT' : 'Austria',
+    'AZ' : 'Azerbaijan',
+    'BS' : 'Bahamas',
+    'BH' : 'Bahrain',
+    'BD' : 'Bangladesh',
+    'BB' : 'Barbados',
+    'BY' : 'Belarus',
+    'BE' : 'Belgium',
+    'BZ' : 'Belize',
+    'BJ' : 'Benin',
+    'BM' : 'Bermuda',
+    'BT' : 'Bhutan',
+    'BO' : 'Bolivia',
+    'BA' : 'Bosnia And Herzegovina',
+    'BW' : 'Botswana',
+    'BV' : 'Bouvet Island',
+    'BR' : 'Brazil',
+    'IO' : 'British Indian Ocean Territory',
+    'BN' : 'Brunei Darussalam',
+    'BG' : 'Bulgaria',
+    'BF' : 'Burkina Faso',
+    'BI' : 'Burundi',
+    'KH' : 'Cambodia',
+    'CM' : 'Cameroon',
+    'CA' : 'Canada',
+    'CV' : 'Cape Verde',
+    'KY' : 'Cayman Islands',
+    'CF' : 'Central African Republic',
+    'TD' : 'Chad',
+    'CL' : 'Chile',
+    'CN' : 'China',
+    'CX' : 'Christmas Island',
+    'CC' : 'Cocos (Keeling) Islands',
+    'CO' : 'Colombia',
+    'KM' : 'Comoros',
+    'CG' : 'Congo',
+    'CD' : 'Congo, Democratic Republic',
+    'CK' : 'Cook Islands',
+    'CR' : 'Costa Rica',
+    'CI' : 'Cote D\'Ivoire',
+    'HR' : 'Croatia',
+    'CU' : 'Cuba',
+    'CY' : 'Cyprus',
+    'CZ' : 'Czech Republic',
+    'DK' : 'Denmark',
+    'DJ' : 'Djibouti',
+    'DM' : 'Dominica',
+    'DO' : 'Dominican Republic',
+    'EC' : 'Ecuador',
+    'EG' : 'Egypt',
+    'SV' : 'El Salvador',
+    'GQ' : 'Equatorial Guinea',
+    'ER' : 'Eritrea',
+    'EE' : 'Estonia',
+    'ET' : 'Ethiopia',
+    'FK' : 'Falkland Islands (Malvinas)',
+    'FO' : 'Faroe Islands',
+    'FJ' : 'Fiji',
+    'FI' : 'Finland',
+    'FR' : 'France',
+    'GF' : 'French Guiana',
+    'PF' : 'French Polynesia',
+    'TF' : 'French Southern Territories',
+    'GA' : 'Gabon',
+    'GM' : 'Gambia',
+    'GE' : 'Georgia',
+    'DE' : 'Germany',
+    'GH' : 'Ghana',
+    'GI' : 'Gibraltar',
+    'GR' : 'Greece',
+    'GL' : 'Greenland',
+    'GD' : 'Grenada',
+    'GP' : 'Guadeloupe',
+    'GU' : 'Guam',
+    'GT' : 'Guatemala',
+    'GG' : 'Guernsey',
+    'GN' : 'Guinea',
+    'GW' : 'Guinea-Bissau',
+    'GY' : 'Guyana',
+    'HT' : 'Haiti',
+    'HM' : 'Heard Island & Mcdonald Islands',
+    'VA' : 'Holy See (Vatican City State)',
+    'HN' : 'Honduras',
+    'HK' : 'Hong Kong',
+    'HU' : 'Hungary',
+    'IS' : 'Iceland',
+    'IN' : 'India',
+    'ID' : 'Indonesia',
+    'IR' : 'Iran, Islamic Republic Of',
+    'IQ' : 'Iraq',
+    'IE' : 'Ireland',
+    'IM' : 'Isle Of Man',
+    'IL' : 'Israel',
+    'IT' : 'Italy',
+    'JM' : 'Jamaica',
+    'JP' : 'Japan',
+    'JE' : 'Jersey',
+    'JO' : 'Jordan',
+    'KZ' : 'Kazakhstan',
+    'KE' : 'Kenya',
+    'KI' : 'Kiribati',
+    'KR' : 'Korea',
+    'KW' : 'Kuwait',
+    'KG' : 'Kyrgyzstan',
+    'LA' : 'Lao People\'s Democratic Republic',
+    'LV' : 'Latvia',
+    'LB' : 'Lebanon',
+    'LS' : 'Lesotho',
+    'LR' : 'Liberia',
+    'LY' : 'Libyan Arab Jamahiriya',
+    'LI' : 'Liechtenstein',
+    'LT' : 'Lithuania',
+    'LU' : 'Luxembourg',
+    'MO' : 'Macao',
+    'MK' : 'Macedonia',
+    'MG' : 'Madagascar',
+    'MW' : 'Malawi',
+    'MY' : 'Malaysia',
+    'MV' : 'Maldives',
+    'ML' : 'Mali',
+    'MT' : 'Malta',
+    'MH' : 'Marshall Islands',
+    'MQ' : 'Martinique',
+    'MR' : 'Mauritania',
+    'MU' : 'Mauritius',
+    'YT' : 'Mayotte',
+    'MX' : 'Mexico',
+    'FM' : 'Micronesia, Federated States Of',
+    'MD' : 'Moldova',
+    'MC' : 'Monaco',
+    'MN' : 'Mongolia',
+    'ME' : 'Montenegro',
+    'MS' : 'Montserrat',
+    'MA' : 'Morocco',
+    'MZ' : 'Mozambique',
+    'MM' : 'Myanmar',
+    'NA' : 'Namibia',
+    'NR' : 'Nauru',
+    'NP' : 'Nepal',
+    'NL' : 'Netherlands',
+    'AN' : 'Netherlands Antilles',
+    'NC' : 'New Caledonia',
+    'NZ' : 'New Zealand',
+    'NI' : 'Nicaragua',
+    'NE' : 'Niger',
+    'NG' : 'Nigeria',
+    'NU' : 'Niue',
+    'NF' : 'Norfolk Island',
+    'MP' : 'Northern Mariana Islands',
+    'NO' : 'Norway',
+    'OM' : 'Oman',
+    'PK' : 'Pakistan',
+    'PW' : 'Palau',
+    'PS' : 'Palestinian Territory, Occupied',
+    'PA' : 'Panama',
+    'PG' : 'Papua New Guinea',
+    'PY' : 'Paraguay',
+    'PE' : 'Peru',
+    'PH' : 'Philippines',
+    'PN' : 'Pitcairn',
+    'PL' : 'Poland',
+    'PT' : 'Portugal',
+    'PR' : 'Puerto Rico',
+    'QA' : 'Qatar',
+    'RE' : 'Reunion',
+    'RO' : 'Romania',
+    'RU' : 'Russian Federation',
+    'RW' : 'Rwanda',
+    'BL' : 'Saint Barthelemy',
+    'SH' : 'Saint Helena',
+    'KN' : 'Saint Kitts And Nevis',
+    'LC' : 'Saint Lucia',
+    'MF' : 'Saint Martin',
+    'PM' : 'Saint Pierre And Miquelon',
+    'VC' : 'Saint Vincent And Grenadines',
+    'WS' : 'Samoa',
+    'SM' : 'San Marino',
+    'ST' : 'Sao Tome And Principe',
+    'SA' : 'Saudi Arabia',
+    'SN' : 'Senegal',
+    'RS' : 'Serbia',
+    'SC' : 'Seychelles',
+    'SL' : 'Sierra Leone',
+    'SG' : 'Singapore',
+    'SK' : 'Slovakia',
+    'SI' : 'Slovenia',
+    'SB' : 'Solomon Islands',
+    'SO' : 'Somalia',
+    'ZA' : 'South Africa',
+    'GS' : 'South Georgia And Sandwich Isl.',
+    'ES' : 'Spain',
+    'LK' : 'Sri Lanka',
+    'SD' : 'Sudan',
+    'SR' : 'Suriname',
+    'SJ' : 'Svalbard And Jan Mayen',
+    'SZ' : 'Swaziland',
+    'SE' : 'Sweden',
+    'CH' : 'Switzerland',
+    'SY' : 'Syrian Arab Republic',
+    'TW' : 'Taiwan',
+    'TJ' : 'Tajikistan',
+    'TZ' : 'Tanzania',
+    'TH' : 'Thailand',
+    'TL' : 'Timor-Leste',
+    'TG' : 'Togo',
+    'TK' : 'Tokelau',
+    'TO' : 'Tonga',
+    'TT' : 'Trinidad And Tobago',
+    'TN' : 'Tunisia',
+    'TR' : 'Turkey',
+    'TM' : 'Turkmenistan',
+    'TC' : 'Turks And Caicos Islands',
+    'TV' : 'Tuvalu',
+    'UG' : 'Uganda',
+    'UA' : 'Ukraine',
+    'AE' : 'United Arab Emirates',
+    'GB' : 'United Kingdom',
+    'US' : 'United States',
+    'UM' : 'United States Outlying Islands',
+    'UY' : 'Uruguay',
+    'UZ' : 'Uzbekistan',
+    'VU' : 'Vanuatu',
+    'VE' : 'Venezuela',
+    'VN' : 'Viet Nam',
+    'VG' : 'Virgin Islands, British',
+    'VI' : 'Virgin Islands, U.S.',
+    'WF' : 'Wallis And Futuna',
+    'EH' : 'Western Sahara',
+    'YE' : 'Yemen',
+    'ZM' : 'Zambia',
+    'ZW' : 'Zimbabwe'
+},
       checked: {it: true, ca: true, ke: false}
     }
   },
 
   render: function() {
     var self = this; 
-    var countries = this.props.countries.map(function(country, i){
-      var countryIcon = "mg map-" + country.abbr;
-      var checkType = self.state.checked[country.abbr] ? 'checked' : 'unchecked';
-      var checkIcon = self.state.checked[country.abbr] ? "fa fa-check-square-o pull-right" : "fa fa-square-o pull-right";
-      return (
-        <ListGroupItem > <i className={countryIcon}></i> {country.name} <i className={checkIcon} onClick={self.handleClick.bind(this, country)}></i> </ListGroupItem>
-      );
+
+    var countries = [];
+
+    $.each(this.state.countries, function(key, value) {
+        var key = key.toLowerCase();
+        var countryIcon = "mg map-" + key;
+        var checkType = self.state.checked[key] ? 'checked' : 'unchecked';
+        var checkIcon = self.state.checked[key] ? "fa fa-check-square-o pull-right" : "fa fa-square-o pull-right";
+        var listItem = <ListGroupItem > <i className={countryIcon}></i> {value} <i className={checkIcon} onClick={self.handleClick.bind(this, key)}></i> </ListGroupItem>;
+
+        countries.push(listItem);
     });
 
-    //generate countryCode colors;
     var self = this;
     var countryColors = {};
-    this.props.countries.map(function(country){
-      var countryCode = country.abbr;
+    $.each(this.state.countries, function(key, value){
+      var countryCode = key.toLowerCase();
       var statusColor = self.state.checked[countryCode] ? "#A5D6A7" : "white";
       var updatedCountry = {}
-      var countryCode = country.abbr.toUpperCase();
+      var countryCode = key;
       var result = countryColors[countryCode] = statusColor;
     });
+
+
+    // $.each(this.state.countries, function(key, value) {
+    //     var key = key.toLowerCase();
+    //     var countryIcon = "mg map-" + key;
+    //     var checkType = self.state.checked[key] ? 'checked' : 'unchecked';
+    //     var checkIcon = self.state.checked[key] ? "fa fa-check-square-o pull-right" : "fa fa-square-o pull-right";
+    //     var listItem = <ListGroupItem > <i className={countryIcon}></i> {value} <i className={checkIcon} onClick={self.handleClick.bind(this, key)}></i> </ListGroupItem>;
+
+    //     countries.push(listItem);
+    // });
 
 
     return (
@@ -40,6 +300,13 @@ var ListInstance = React.createClass({
         <div id="map">
           <WorldMap countries={this.props.countries} checkedCountries={this.state.checked} countryColors={countryColors}/>
         </div>
+        <select className="form-control">
+          <option value="Africa">Africa</option>
+          <option value="The Americas">The Americas</option>
+          <option value="Europe">Europe</option>
+          <option value="Asia">Asia</option>
+          <option value="Australia and the Pacific">Australia and the Pacific</option>
+        </select>
         <ListGroup>
           {countries}
         </ListGroup>
@@ -47,14 +314,14 @@ var ListInstance = React.createClass({
     );
   },
 
-  handleClick: function(country) {
+  handleClick: function(abbr) {
       var checked = this.state.checked;
-      if (!checked[country.abbr]){
-        checked[country.abbr] = true;
-        this.setState({checked: checked});        
+      if (!checked[abbr]){
+        checked[abbr] = true;
+        this.setState({checked: checked});    
       }
       else{
-        checked[country.abbr] = false;
+        checked[abbr] = false;
         this.setState({checked: checked});
       }
   }
@@ -63,7 +330,6 @@ var ListInstance = React.createClass({
 
 var WorldMap = React.createClass({
   componentDidMount: function() {
-    // console.log(this.getDOMNode()); 
     this.worldMap = new jvm.Map({
       map: 'world_mill_en',
       backgroundColor: "#0288D1",
