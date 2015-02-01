30efa38dea38000bc654d899f8561405c7aac8e4
Krista Williams <rystakei@gmail.com>
2015-02-01 08:25:41 -0500

    added handleClick function to toggle between checked and unchecked

---
+++ app/scripts/reactmain.jsx
@@ -0,0 +1,59 @@
+var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+var Button = ReactBootstrap.Button;
+var Grid = ReactBootstrap.Grid;
+var ListGroup = ReactBootstrap.ListGroup;
+var ListGroupItem = ReactBootstrap.ListGroupItem;
+
+
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
+
+var buttonsInstance = (
+    <div className="well" style={wellStyles}>
+      <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
+      <Button bsSize="large" block>Block level button</Button>
+    </div>
+  );
+
+var ListInstance = React.createClass({
+
+  getInitialState: function() {
+    return {
+      checked: {it: true, ca: true, ke: false}
+    }
+  },
+
+  render: function() {
+    var self = this; 
+    var countries = this.props.countries.map(function(country, i){
+      var checkType = self.state.checked[country.abbr] ? 'checked' : 'unchecked';
+      return (
+        <ListGroupItem onClick={self.handleClick.bind(this, country)}> {country.name} {checkType} </ListGroupItem>
+      );
+    });
+    return (
+      <ListGroup>
+        {countries}
+      </ListGroup>
+    );
+  },
+
+  handleClick: function(country) {
+      var checked = this.state.checked;
+      if (!checked[country.abbr]){
+        checked[country.abbr] = true;
+        this.setState({checked: checked});        
+      }
+      else{
+        checked[country.abbr] = false;
+        this.setState({checked: checked});
+      }
+  }
+
+});
+
+React.render(<ListInstance countries={[{name:'Italy', abbr: 'it', checked: false}, {name: 'Canada', abbr: 'ca', checked: false}, {name: 'Kenya', abbr: 'ke', checked: true}]} />, document.getElementById('reactStuff'));
