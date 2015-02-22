47ca57577d19ac9820b34008ec7cd359cace410d
Krista Williams <rystakei@gmail.com>
2015-02-22 07:51:15 -0500

    Integrated with Rails API so that state is based on what is retrieved from the API

--- app/scripts/reactmain.jsx
+++ app/scripts/reactmain.jsx
@@ -520,34 +520,65 @@ var ListInstance = React.createClass({
     'ZM' : 'Zambia',
     'ZW' : 'Zimbabwe'
 },
-      checked: {it: true, ca: true, ke: false}
+      checked: {it: true, ca: true, ke: false},
+
+      countryData: {}
     }
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+
+      url: this.props.url,
+      type: "GET",
+      crossDomain: true,
+      dataType: 'JSON',
+      success: function(data) {
+        this.setState({countryData: data});
+
+        var checked = {};
+        $.each(data, function(i,country){
+            if (country.checked === true) {
+                var countryCode = country.code;
+                checked[countryCode]=true;
+            }
+        });
+       this.setState({checked: checked});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
 
   render: function() {
     var self = this; 
-
     var countries = [];
+    var countryColors = {};
 
-    $.each(this.state.countries, function(key, value) {
-        var key = key.toLowerCase();
-        var countryIcon = "mg map-" + key;
-        var checkType = self.state.checked[key] ? 'checked' : 'unchecked';
-        var checkIcon = self.state.checked[key] ? "fa fa-check-square-o pull-right" : "fa fa-square-o pull-right";
-        var countryStats = <div className="country-stats"> <p><b>30</b> Italian restaurants near <a href="#"><b>02143</b></a></p></div>;
-        var wikiPage = <div className="wiki-page"><a href="#"> Learn about Italian cuisine on Wikipedia </a></div>;
-        var listItem = <ListGroupItem className="country-item"> <i className={countryIcon}></i> {value} <i className={checkIcon} onClick={self.handleClick.bind(this, key)}></i> {countryStats} {wikiPage} </ListGroupItem>;
+    $.each(this.state.countryData, function(i, country) {
+        var countryCode = country.code;
+        var countryCodeLC = country.code.toLowerCase();
+        var countryName = country.name;
+        var demonym = country.demonym;
+        var countryIcon = "mg map-" + countryCodeLC;
+        var checkType = self.state.checked[countryCode] ? 'checked' : 'unchecked';
+        var checkIcon = self.state.checked[countryCode] ? "fa fa-check-square-o pull-right" : "fa fa-square-o pull-right";
+        var countryStats = <div className="country-stats"> <p><b>30</b> {demonym} restaurants near <a href="#"><b>02143</b></a></p></div>;
+        var wikiPage = <div className="wiki-page"><a href="#"> Learn about {demonym} cuisine on Wikipedia </a></div>;
+        var listItem = <ListGroupItem className="country-item"> <i className={countryIcon}></i> {countryName} <i className={checkIcon} onClick={self.handleClick.bind(this, countryCode.toUpperCase())}></i> {countryStats} {wikiPage} </ListGroupItem>;
 
         countries.push(listItem);
+
+        var statusColor = self.state.checked[country.code] ? "#A5D6A7" : "white";
+        var result = countryColors[country.code] = statusColor;
+
     });
 
-    var self = this;
-    var countryColors = {};
-    $.each(this.state.countries, function(key, value){
-      var countryCode = key.toLowerCase();
+    $.each(this.state.countryData, function(i, country){
+      var countryCode = country.code.toUpperCase();
       var statusColor = self.state.checked[countryCode] ? "#A5D6A7" : "white";
       var updatedCountry = {}
-      var countryCode = key;
       var result = countryColors[countryCode] = statusColor;
     });
 
@@ -608,7 +639,7 @@ filterList: function(region) {
       var checked = this.state.checked;
       if (!checked[abbr]){
         checked[abbr] = true;
-        this.setState({checked: checked});    
+        this.setState({checked: checked});  
       }
       else{
         delete checked[abbr];
@@ -673,6 +704,6 @@ var WorldMap = React.createClass({
   }
 });
 
-React.render(<ListInstance countries={[{name:'Italy', abbr: 'it', checked: false}, {name: 'Canada', abbr: 'ca', checked: false}, {name: 'Kenya', abbr: 'ke', checked: true}]} />, document.getElementById('myApp'));
+React.render(<ListInstance url={"http://localhost:3000/countries.json"} countries={[{name:'Italy', abbr: 'it', checked: false}, {name: 'Canada', abbr: 'ca', checked: false}, {name: 'Kenya', abbr: 'ke', checked: true}]} />, document.getElementById('myApp'));
 
 exports.React = window.React = React;
